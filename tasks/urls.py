18f3ac2736486b2a8adc4c14431378008a310f16
WilliamRincon02 <rinconwilliam996@gmail.com>
2024-04-23 14:24:13 -0500

    feat: add templates json and new view users

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, users
 
 urlpatterns = [
     path("", index, name="index"),
+    path("users", users, name="users"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.core.serializers import serialize
+from django.http import JsonResponse
+
+from .models import Task
+from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("Hello People")
+    tasks = Task.objects.all()
+    data = serialize('python', tasks)
+    return JsonResponse(data, safe=False)
+
+def users(request):
+    users = User.objects.all()
+    data = serialize('python', users)
+    return JsonResponse(data, safe=False)
