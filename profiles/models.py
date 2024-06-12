5108ee7654ade205a01a208e084defa94a3ce805
WilliamRincon02 <rinconwilliam996@gmail.com>
2024-06-12 09:14:14 -0500

    fix: filtering implementation and the token is removed from the user model

--- profiles/models.py
+++ profiles/models.py
@@ -11,10 +11,10 @@ from rest_framework.authtoken.models import Token
 
 class Profile(AbstractUser):
     bio = models.TextField(verbose_name="Biography", null=True, blank=True)
-    jwt_access_token = models.TextField(blank=True, null=True)
-    jwt_refresh_token = models.TextField(blank=True, null=True)
+    # jwt_access_token = models.TextField(blank=True, null=True)
+    # jwt_refresh_token = models.TextField(blank=True, null=True)
     
-    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-    def create_auth_token(sender, instance=None, created=False, **kwargs):
-        if created:
-            Token.objects.create(user=instance)
+    # @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    # def create_auth_token(sender, instance=None, created=False, **kwargs):
+    #     if created:
+    #         Token.objects.create(user=instance)
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from .models import Task
 from tasks.serializers import TaskSerializer
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -9,7 +10,8 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [OrderingFilter]
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['completed']
     ordering_fields = ["priority", "completed"]
 
     def get_queryset(self):
